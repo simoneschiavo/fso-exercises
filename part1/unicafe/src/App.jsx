1aa6cee0d9a1807c1496e87eadea93b27564f555
Xformance Development <simoneschiavo@icloud.com>
2025-07-21 16:58:03 +0200

    Add solutions for exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,23 +6,36 @@ const Button = (props) => {
 
 const StatisticLine = (props) => {
   return (
-    <p>{props.text} {props.statistic}</p>
-  )
-}
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.statistic}</td>
+    </tr>
+  );
+};
 
 const Statistics = (props) => {
   return (
     <>
       <h1>statistics</h1>
-      <StatisticLine text="good" statistic={props.good} />
-      <StatisticLine text="neutral" statistic={props.neutral} />
-      <StatisticLine text="bad" statistic={props.bad} />
-      <StatisticLine text="all" statistic={props.all} />
-      <StatisticLine text="average" statistic={Math.round((props.value / props.all) * 100) / 100} />
-      <StatisticLine text="positive" statistic={Math.round((props.good / props.all) * 10000) / 100 + "%"} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" statistic={props.good} />
+          <StatisticLine text="neutral" statistic={props.neutral} />
+          <StatisticLine text="bad" statistic={props.bad} />
+          <StatisticLine text="all" statistic={props.all} />
+          <StatisticLine
+            text="average"
+            statistic={Math.round((props.value / props.all) * 100) / 100}
+          />
+          <StatisticLine
+            text="positive"
+            statistic={Math.round((props.good / props.all) * 10000) / 100 + "%"}
+          />
+        </tbody>
+      </table>
     </>
   );
-}
+};
 
 const App = () => {
   const [good, setGood] = useState(0);
@@ -34,21 +47,38 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => {
-        setGood(good + 1);
-        setAll(all + 1);
-        setValue(value + 1);
-      }} text="good" />
-      <Button handleClick={() => {
-        setNeutral(neutral + 1);
-        setAll(all + 1);
-      }} text="neutral" />
-      <Button handleClick={() => {
-        setBad(bad + 1);
-        setAll(all + 1);
-        setValue(value - 1);
-      }} text="bad" />
-      {all>0 && <Statistics good={good} neutral={neutral} bad={bad} all={all} value={value} />}
+      <Button
+        handleClick={() => {
+          setGood(good + 1);
+          setAll(all + 1);
+          setValue(value + 1);
+        }}
+        text="good"
+      />
+      <Button
+        handleClick={() => {
+          setNeutral(neutral + 1);
+          setAll(all + 1);
+        }}
+        text="neutral"
+      />
+      <Button
+        handleClick={() => {
+          setBad(bad + 1);
+          setAll(all + 1);
+          setValue(value - 1);
+        }}
+        text="bad"
+      />
+      {all > 0 && (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          all={all}
+          value={value}
+        />
+      )}
     </div>
   );
 };
